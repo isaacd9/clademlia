e3bebb80b1992434948ac2394832d1e2a44695eb
Isaac Diamond <isaacd9@gmail.com>
2016-12-31 22:23:46 -0800

    Start on ping

--- clademlia-core/src/thrift/schema.thrift
+++ clademlia-core/src/thrift/schema.thrift
@@ -1,5 +1,41 @@
 namespace java clademlia.thrift
 
+struct PingMessage {
+  1: required binary rpcId;
+  2: required binary nodeId;
+  3: optional string ip;
+  4: optional i16 port;
+}
+
+struct StoreRequest {
+  1: required binary nodeId;
+  2: required binary rpcId;
+  3: required string key;
+  4: required binary value;
+}
+
+struct StoreResponse {
+  1: required binary nodeId;
+  2: required binary rpcId;
+  3: required bool result;
+}
+
+struct FindRequest {
+  1: required string key;
+}
+
+struct FindResponse {
+  1: required string ip;
+  2: required i16 port;
+  3: required binary nodeId;
+  4: optional binary value;
+}
+
+typedef list<FindResponse> FindResponseList
+
 service Clademlia {
-    // TODO
+  PingMessage ping(1: PingMessage msg)
+  StoreResponse store(1: StoreRequest req)
+  FindResponseList findNode(1: FindRequest req)
+  FindResponseList findValue(1: FindRequest req)
 }
--- clademlia-service/src/clademlia/service.clj
+++ clademlia-service/src/clademlia/service.clj
@@ -1,15 +1,55 @@
 (ns clademlia.service
-  (:import [clademlia.thrift Clademlia])
+  (:import [clademlia.thrift Clademlia java.net NetworkInterface])
   (:require [finagle-clojure.futures :as f]
             [finagle-clojure.thrift :as thrift])
   (:gen-class))
 
+(def ip
+  (.getHostAddress
+    (java.net.InetAddress/getLocalHost)))
+
+(def distance bit-xor)
+
+(defn updateNode
+  [nodeId ip port]
+  (do
+    (println "Updating")
+    (println nodeId)
+    (println ip)
+    (println port)
+  )
+)
+
+(defn ping
+  [msg nid port]
+  (let [rpcId (.getRpcId msg)
+        otherNodeId (.getNodeId msg)
+        otherIp (.getIp msg)
+        otherPort (.getPort msg)]
+        (do
+          (updateNode otherNodeId otherIp otherPort)
+          (f/value (PingMessage. rpcId nid ip))
+        )
+    )
+)
+
 (defn make-service
-  []
+  [nid port]
   (thrift/service Clademlia
-    ;; TODO implement service methods
+    (ping [msg nid port])
     ))
 
 (defn -main
-  [& args]
-  (f/await (thrift/serve ":9999" (make-service))))
+  [ip port nid & args]
+  (do
+    (f/await
+      (thrift/serve
+        (format ":%s" port)
+        (make-service nid port)
+        )
+      )
+    (println
+      (format "Now serving on %s" port)
+    )
+  )
+)
